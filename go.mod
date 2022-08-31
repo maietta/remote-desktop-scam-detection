ebd741d02bc68cd6ecaa2162b84a2005a0f6ce8c
Nick Maietta <nick@premoweb.com>
2022-08-31 14:39:35 -0700

    POF working - One tiny bug

--- go.mod
+++ go.mod
@@ -2,9 +2,21 @@ module premoweb/scamdetect
 
 go 1.18
 
-require golang.org/x/sys v0.0.0-20220825204002-c680a09ffe64
+require (
+	github.com/mitchellh/go-ps v1.0.0
+	github.com/pborman/getopt v1.1.0
+	golang.org/x/sys v0.0.0-20220825204002-c680a09ffe64
+)
 
 require (
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/shirou/gopsutil/v3 v3.22.7 // indirect
+	github.com/tklauser/go-sysconf v0.3.10 // indirect
+	github.com/tklauser/numcpus v0.4.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
 	gopkg.in/toast.v1 v1.0.0-20180812000517-0a84660828b2 // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
 	"log"
+	"os"
+	"os/exec"
 	"strings"
 	"syscall"
 	"time"
 	"unsafe"
 
+	"github.com/mitchellh/go-ps"
 	"golang.org/x/sys/windows"
 	"gopkg.in/toast.v1"
 )
@@ -24,9 +30,7 @@ type (
 )
 
 func GetWindowTextLength(hwnd HWND) int {
-	ret, _, _ := procGetWindowTextLength.Call(
-		uintptr(hwnd))
-
+	ret, _, _ := procGetWindowTextLength.Call(uintptr(hwnd))
 	return int(ret)
 }
 
@@ -53,36 +57,150 @@ func GetAsyncKeyState(vKey int) int16 {
 	return int16(ret)
 }
 
+// func PS() {
+// 	ps, _ := ps.Processes()
+// 	fmt.Println(ps[0].Executable())
+
+// 	for pp := range ps {
+// 		fmt.Printf("%d %s\n", ps[pp].Pid(), ps[pp].Executable())
+// 	}
+// }
+
+// FindProcess( key string ) ( int, string, error )
+func FindProcess(key string) (int, string, error) {
+	pname := ""
+	pid := 0
+	err := errors.New("not found")
+	ps, _ := ps.Processes()
+
+	for i, _ := range ps {
+		if ps[i].Executable() == key {
+			pid = ps[i].Pid()
+			pname = ps[i].Executable()
+			err = nil
+			break
+		}
+	}
+	return pid, pname, err
+}
+
+func killProcessByName(procname string) int {
+	kill := exec.Command("taskkill", "/im", procname, "/T", "/F")
+	err := kill.Run()
+	if err != nil {
+		return -1
+	}
+	return 0
+}
+
+func trigger() {
+
+	usualSuspects := []string{
+		"AnyDesk.exe",
+		"RustDesk.exe",
+		"TeamViewer.exe",
+	}
+
+	for _, suspect := range usualSuspects {
+		if pid, s, err := FindProcess(suspect); err == nil {
+			fmt.Printf("Pid:%d, Pname:%s\n", pid, s)
+
+			// Kill the process
+			killProcessByName(suspect)
+		}
+	}
+	// ok := dialog.Message("%s", "Do you want to continue?").Title("Are you sure?").YesNo()
+	// if ok {
+	// 	// Send notification
+
+	notification := toast.Notification{
+		AppID:   "Scam Detection Monitor",
+		Title:   "Possible Scam in Progress!",
+		Message: "If you are seeing this after being receiving a call, it appears you may be a victim of a scam. Please hang up and call the company directly to verify the legitimacy of the call.",
+	}
+	err := notification.Push()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
+
+func contains(elems []string, v string) bool {
+	for _, s := range elems {
+		if v == s {
+			return true
+		}
+	}
+	return false
+}
+
+func buildListFromFile() []string {
+	// Open the wordlist.txt file
+	file, err := os.Open("wordlist.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Read the file
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines
+
+}
+
 func main() {
+
+	// Build the list of words to look for
+	words := buildListFromFile()
+
 	for {
 		if hwnd := getWindow("GetForegroundWindow"); hwnd != 0 {
 			text := GetWindowText(HWND(hwnd))
 
-			// fmt.Println("window :", text, "# hwnd:", hwnd)
+			//browsers := [4]string{"Brave", "Gogle Chrome", "Microsoft Edge", "Mozilla Firefox"}
 
-			if strings.Contains(text, "Brave") || strings.Contains(text, "Google Chrome") || strings.Contains(text, "Microsoft Edge") || strings.Contains(text, "Firefox") {
+			// The tab name is anything after the last dash
+			tabName := strings.Split(text, " - ")[len(strings.Split(text, " - "))-1]
 
-				// Detect if F12 is pressed
-				if GetAsyncKeyState(123) != 0 {
+			// Trim the whitespace
+			tabName = strings.TrimSpace(tabName)
 
-					notification := toast.Notification{
-						AppID:   "Scam Detection Monitor",
-						Title:   "Possible Scam in Progress!",
-						Message: "If you are seeing this after being receiving a call, it appears you may be a victim of a scam. Please hang up and call the company directly to verify the legitimacy of the call.",
-						// Icon:    "go.png", // This file must exist (remove this line if it doesn't)
-						Actions: []toast.Action{
-							{"protocol", "Disconnect Scammer", ""},
-							{"protocol", "Learn More", ""},
-						},
+			// Does browsers contain the tab name?
+			if contains([]string{"Brave", "Gogle Chrome", "Microsoft​ Edge", "Mozilla Firefox"}, tabName) {
+
+				// Remove the browser name from the tab name
+				// Remove tabName from text
+				data := strings.Replace(text, " - "+tabName, "", -1)
+				data = strings.ToLower(data)
+
+				// split the data into words
+				dataWords := strings.Split(data, " ")
+
+				// Check if any of the words in the dataWords slice are in the words slice
+				for _, word := range dataWords {
+					if contains(words, word) {
+
+						// If CTRL + SHIFT + i is pressed.
+						if GetAsyncKeyState(0x10) != 0 && GetAsyncKeyState(0x11) != 0 && GetAsyncKeyState(0x49) != 0 {
+							trigger()
+							time.Sleep(1 * time.Second)
+
+							// If or F12 is pressed.
+						} else if GetAsyncKeyState(123) != 0 {
+							trigger()
+							time.Sleep(1 * time.Second)
+
+						}
+						break
 					}
-					err := notification.Push()
-					if err != nil {
-						log.Fatalln(err)
-					}
-
-					time.Sleep(2 * time.Second)
-
 				}
+
 			}
 
 		}
